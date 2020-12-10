5dc3c0949f1da11ae14bb5c5d4928aaf5814af65
yuji okayama <yujiokayama@yujinoMacBook-Pro.local>
2020-12-11 00:28:17 +0900

    update

--- components/examples/progress/ProgressCircle.vue
+++ components/examples/progress/ProgressCircle.vue
@@ -1,31 +1,18 @@
 <template>
-  <div>
-    {{ currentAngle }}
-    {{ leftAngle }}
-    {{ rightAngle }}
-
-    <div style="position: relative; width: 200px; height: 200px">
-      <!-- 右側の180度分の領域 -->
-      <div class="square" style="right: 0">
-        <div
-          class="square"
-          style="
-            right: 100%;
-            transform-origin: 100% 50%;
-            transform: rotate(180deg);
-          "
-        >
-          <div class="circle" style="left: 0"></div>
-        </div>
+  <div class="circle-container">
+    <!-- right -->
+    <div class="square square-right">
+      <div class="square" :style="rightAngle">
+        <div class="circle-inner square-left"></div>
       </div>
-      <!-- 左側の180度分の領域 -->
-      <div class="square" style="left: 0">
-        <div
-          class="square"
-          style="left: 100%; transform-origin: 0 50%; transform: rotate(30deg)"
-        >
-          <div class="circle" style="right: 0"></div>
-        </div>
+    </div>
+    <div class="circle-text">
+      {{ percentView }}
+    </div>
+    <!-- left -->
+    <div class="square" style="left: 0">
+      <div class="square" :style="[leftAngle]">
+        <div class="circle-inner square-right"></div>
       </div>
     </div>
   </div>
@@ -36,23 +23,30 @@ export default {
   props: {
     percent: {
       type: Number,
-      default: 100,
+      default: 10,
       required: false,
     },
   },
   computed: {
+    percentView() {
+      return `${this.percent}%`
+    },
     currentAngle() {
       return Math.floor((360 * this.percent) / 100)
     },
     rightAngle() {
       const angle = Math.min(this.currentAngle, 180)
       return {
+        right: '100%',
+        'transform-origin': '100% 50%',
         transform: `rotate(${angle}deg)`,
       }
     },
     leftAngle() {
       const angle = Math.min(Math.max(this.currentAngle - 180, 0), 180)
       return {
+        left: '100%',
+        'transform-origin': '0 50%',
         transform: `rotate(${angle}deg)`,
       }
     },
@@ -65,17 +59,38 @@ export default {
 
 <style lang="scss" scoped>
 .square {
+  &-right {
+    right: 0;
+  }
+  &-left {
+    left: 0;
+  }
   position: absolute;
   width: 100px;
   height: 200px;
   overflow: hidden;
 }
 .circle {
-  position: absolute;
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  border: 10px solid green;
-  box-sizing: border-box;
+  &-container {
+    position: relative;
+    width: 200px;
+    height: 200px;
+  }
+  &-inner {
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    border: 10px solid lightskyblue;
+    box-sizing: border-box;
+  }
+  &-text {
+    position: absolute;
+    width: 200px;
+    line-height: 200px;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+  }
 }
 </style>
