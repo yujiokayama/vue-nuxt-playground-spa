8aaeb781744b549d80ba9ab6cb2ac12e12b6cb94
yuji okayama <yujiokayama@yujinoMacBook-Pro.local>
2020-12-12 18:10:02 +0900

    update

--- components/examples/crud/CRUD.vue
+++ components/examples/crud/CRUD.vue
@@ -1,19 +1,36 @@
 <template>
   <div>
-    {{ post }}
-    <button @click="fetchPost">API取得</button>
+    {{ posts }}
+    <button
+      class="mt-5 bg-blue-700 text-white border border-blue-700 font-bold py-2 px-6 rounded-lg"
+      @click="fetchPost"
+    >
+      API取得
+    </button>
+
+    {{ create }}
+    <button
+      class="mt-5 bg-blue-700 text-white border border-blue-700 font-bold py-2 px-6 rounded-lg"
+      @click="createPost"
+    >
+      新規登録
+    </button>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      post: [],
+      posts: [],
+      create: {},
     }
   },
   methods: {
     async fetchPost() {
-      this.post = await this.$api.getAPI()
+      this.posts = await this.$api.getAPI()
+    },
+    async createPost() {
+      this.create = await this.$api.postAPI()
     },
   },
 }
